14487b2778b693998413afc73c3031e6e0136bfc
Bettyz01 <xinranzhang021009@gmail.com>
2023-04-13 12:32:37 -0700

    visualizations analysis and observations added

--- analysis/code/Project_Functions_Betty.py
+++ analysis/code/Project_Functions_Betty.py
@@ -1,6 +1,6 @@
 def method_chaining (data):
     
-    data = df_cleaned
+    data
     method_chain_data = (data.dtypes
                         .apply(lambda x: check_delim(data))
                         .apply(lambda x: ~data['EDUCATION'].isin([0, 5, 6]))
