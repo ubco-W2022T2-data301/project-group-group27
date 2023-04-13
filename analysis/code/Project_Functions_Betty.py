fc7c312a964ef195b694aa72ceb52db3c6d09c3a
Bettyz01 <xinranzhang021009@gmail.com>
2023-04-13 13:41:10 -0700

    method chaining edited

--- analysis/code/Project_Functions_Betty.py
+++ analysis/code/Project_Functions_Betty.py
@@ -1,19 +1,18 @@
 import pandas as pd
 import numpy as np
 
-def method_chaining(file):
-    df = (
-        pd.read_excel(io = file)
-        .rename(columns=df.iloc[0])
+def method_chaining(data_methodchaining):
+    dataframe = (
+        data_methodchaining.rename(columns=data_methodchaining.iloc[0])
         .iloc[1:].reset_index()
         .apply(pd.to_numeric)   
     )
     
-    df1 = (
-        df..dropna(axis = 0).drop(columns = 'index')
-        .apply(lambda x: ~data['EDUCATION'].isin([0, 5, 6]))
-        .apply(lambda x: ~data['MARRIAGE'].isin([0]))
+    dataframe1 = (
+        dataframe.dropna(axis = 0).drop(columns = 'index')
+        .apply(lambda x: ~x['EDUCATION'].isin([0, 5, 6]))
+        .apply(lambda x: ~x['MARRIAGE'].isin([0]))
     )
     
-    return df1
+    return dataframe1
 
